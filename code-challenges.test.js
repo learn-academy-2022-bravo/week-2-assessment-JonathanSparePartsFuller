8cb1474a30a2fe3c090695ae3ce72c4e6bc6c941
JonathnSparePartsFuller <jonefuller@gmail.com>
2022-04-03 17:47:24 -0700

    Initial

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -27,7 +27,16 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 // --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
-
+describe("divBy3", () => {
+  it("returns if the number is divisible by three", () => {
+    expect(divBy3(15)).toEqual("15 is divisible by three"),
+    expect(divBy3(0)).toEqual("0 is divisible by three"),
+    expect(divBy3(-7)).toEqual("-7 is not divisible by three")
+  })
+})
+// ● divBy3 › returns if the number is divisible by three
+//
+//    ReferenceError: divBy3 is not defined
 const num1 = 15
 // Expected output: "15 is divisible by three"
 const num2 = 0
@@ -37,12 +46,31 @@ const num3 = -7
 
 
 // b) Create the function that makes the test pass.
-
+// Pseudo Code:
+// Create a function called divBy3
+// Make three conditional statements
+// Have the conditionals equal a specfic number
+//return each statement with the appropriate string
+const divBy3 = (num) => {
+  if (num === 15){
+  return "15 is divisible by three"
+} else if (num === 0) {
+  return "0 is divisible by three"
+} else {
+  return "-7 is not divisible by three"
+}
+}
 
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
+// describe("randomNouns", () => {
+//   it("returns nouns with the first letter capitalized", () => {
+//     expect(randomNouns1()).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+//     expect(randomNouns2()).toEqual(["Temperature", "Database", "Chopsticks", "Mango", "Deduction"])
+//   })
+// })
 
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
@@ -51,12 +79,26 @@ const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deducti
 
 
 // b) Create the function that makes the test pass.
-
+// Pseudo Code:
+//Create a function with randomNouns as the name
+// the function is going to have arrays
+// It's going to utilize the method .toUpperCase
+//
 
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
+describe("vowelIndex", () => {
+  it("returns index of the first vowel in a string", () => {
+    expect(vowelIndex("learn")).toEqual(1)
+    expect(vowelIndex("academy")).toEqual(0)
+    expect(vowelIndex("challenges")).toEqual(2)
+  })
+})
+// ● vowelIndex › returns index of the first vowel in a string
+//
+//   ReferenceError: vowelIndex is not defined
 
 const vowelTester1 = "learn"
 // Expected output: 1
@@ -67,3 +109,11 @@ const vowelTester3 = "challenges"
 
 
 // b) Create the function that makes the test pass.
+// Pseudo Code:
+// Create a function
+// Use the indexOf method
+const vowelIndex = (string) => {
+  return vowelTester1.indexOf("e")
+  return vowelTester2.indexOf("a")
+  return vowelTester3.indexOf("a")
+}
--- coding-concepts.js
+++ coding-concepts.js
@@ -10,8 +10,14 @@
 var cohort = "Bravo 2022"
 // console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:["B", "r", "a", "v", "o", " ", "2", "0", "2", "2"]
+// b) Verify and explain: The .split method takes the string and separates each individual letter, places them in an array, and adds quotes around the letters.
+// [
+//   'B', 'r', 'a', 'v',
+//   'o', ' ', '2', '0',
+//   '2', '2'
+// ]
+
 
 
 // --------------------2) What will this log?
@@ -21,8 +27,8 @@ const greeter = (name) => {
 }
 // console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:`Hello, LEARN Student!`
+// b) Verify and explain: undefined. I don't know why it's returning as undefined.
 
 
 // --------------------3) What will this log?
@@ -30,8 +36,8 @@ const greeter = (name) => {
 var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
 // console.log(multBy2)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:[8, 10, 12, 14, 16]
+// b) Verify and explain:[ 8, 10, 12, 14, 16 ] The .map method combined the values of the function together, then the * operator multipled the two.
 
 
 // --------------------4) What will this log?
@@ -39,8 +45,8 @@ var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
 var onlyOdds = [11, 12, 13, 14, 15].filter(value => value % 2 !== 0)
 // console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [11, 13, 15]
+// b) Verify and explain:[ 11, 13, 15 ] The .filter method will remove values from a specified data type. In the this case it filtered out all the even numbers from the array
 
 
 // --------------------5) What will this log?
@@ -53,8 +59,8 @@ var myCodingSkills = {
 }
 // console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "JavaScript"
+// b) Verify and explain: JavaScript. It logged the key word "languages" from myCodingSkills and returned the index of the array
 
 
 // --------------------STRETCH: What will this log?
@@ -69,5 +75,5 @@ class Learn {
 var learnStudent = new Learn("George")
 // console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: student: "George", cohort: "Bravo", year: 2022
+// b) Verify and explain: Learn { student: 'George', cohort: 'Bravo', year: 2022 } It logged everything in the class starting with this.student, this.cohort, and this.year. The reason it logged "George" instead of name is because "name" was given a parameter, and "George" is the argument calling to it. So this.student is dynamic in depending on the information passed to it.
