a079070182bcc3a011adefb6b11f8dbc5209ddb3
JonathnSparePartsFuller <jonefuller@gmail.com>
2022-04-21 13:24:26 -0700

    Just a test file for coding challenges

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+// const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+// // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+// const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
 
-let string1 = randomNouns1.join(", ")
-let string2 = randomNouns2.join(", ")
-let upperCase1 = string1.charAt(0).toUpperCase()
-let upperCase2 = string2.charAt(0).toUpperCase()
-console.log(upperCase1)
-console.log(upperCase2)
+// let string1 = randomNouns1.join(", ")
+// let string2 = randomNouns2.join(", ")
+// let upperCase1 = string1.charAt(0).toUpperCase()
+// let upperCase2 = string2.charAt(0).toUpperCase()
+// console.log(upperCase1)
+// console.log(upperCase2)
